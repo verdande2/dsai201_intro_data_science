447f9d69d4a358d6108681abbc650001a711a77e
Andrew Sparkes <andrew.sparkes@oit.edu>
2025-10-01 13:54:02 -0700

    added basic import and df test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from dsai201-intro-data-science!")
+import pandas as pd
 
+def main():
+    print("Testing pandas import...")
+    # A is first column, B is second column
+    test_df = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+    print(test_df)
 
 if __name__ == "__main__":
     main()
